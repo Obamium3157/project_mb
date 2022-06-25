ee89eb838e86ab7c2b1560c989f9525c1d6302d4
Petr Zhdanov <p_zhdanov@icloud.com>
2022-06-25 18:48:02 +0300

    Сделан выбор цвета с дальнейшим переносом оного на страницу игры

--- js/col.js
+++ js/col.js
@@ -1,19 +1,3 @@
-var color_id = 0
-var border_id = 0
-
-function set_col(c_id, b_id) {
-    color_id = c_id
-    border_id = b_id
-}
-
-function get_col() {
-    switch (color_id) {
-        case 0: return 'yellow'
-        case 1: return 'lightblue'
-    }
-
-    return 0
-}
-function get_border() {
-    return 1
+function set_col(col) {
+    sessionStorage.setItem('color', col)
 }
