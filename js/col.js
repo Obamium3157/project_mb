43dbc41e76c1b981c0801d1d5648d471e7084590
Petr Zhdanov <p_zhdanov@icloud.com>
2022-06-25 23:31:13 +0300

    smol code changes

--- js/col.js
+++ js/col.js
@@ -1,3 +1,13 @@
 function set_col(col) {
     sessionStorage.setItem('color', col)
+    setImage(sessionStorage.getItem('color'))
+}
+
+function setImage(col) {
+    switch (col) {
+        case 'yellow': document.getElementById('view_color').src = "/img/yel.png"; break;
+        case 'lightblue': document.getElementById('view_color').src = "/img/bb.png"; break;
+        case 'red': document.getElementById('view_color').src = "/img/red.png"; break;
+        case 'blue': document.getElementById('view_color').src = "/img/blue.png"; break;
+    }
 }
