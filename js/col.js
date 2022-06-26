2e4323e850162623c3e0a067c0026c38e178e85f
Petr Zhdanov <p_zhdanov@icloud.com>
2022-06-26 16:48:28 +0300

    По-другому реализован выбор цвета. НУЖНО СДЕЛАТЬ АНИМАЦИЮ УВЕЛИЧЕНИЯ/УМЕНЬШЕНИЯ ЦВЕТОВ ПЛАВНОЙ, РАЗРУЛИТЬ ПРОБЛЕМЫ С РАСПОЛОЖЕНИЕМ

--- js/col.js
+++ js/col.js
@@ -1,13 +1,25 @@
 function set_col(col) {
     sessionStorage.setItem('color', col)
-    setImage(sessionStorage.getItem('color'))
+}
+
+function sizeUP(id) {
+    document.getElementById(id).style.width = "375px"
+    document.getElementById(id).style.height = "375px"
 }
 
 function setImage(col) {
     switch (col) {
-        case 'yellow': document.getElementById('view_color').src = "/img/yel.png"; break;
-        case 'lightblue': document.getElementById('view_color').src = "/img/bb.png"; break;
-        case 'red': document.getElementById('view_color').src = "/img/red.png"; break;
-        case 'blue': document.getElementById('view_color').src = "/img/blue.png"; break;
+        case 'yellow':
+            document.getElementById('view_color').src = "/img/yel.png";
+            break;
+        case 'lightblue':
+            document.getElementById('view_color').src = "/img/bb.png";
+            break;
+        case 'red':
+            document.getElementById('view_color').src = "/img/red.png";
+            break;
+        case 'blue':
+            document.getElementById('view_color').src = "/img/blue.png";
+            break;
     }
 }
